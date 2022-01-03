f59c4dcac7001aef00e050e9b8ae9722f80dc547
Eder Junior Chua <eder.chua@ciit.edu.ph>
2022-01-03 18:41:08 +0800

    added some ComputerComponents on DATA

---
+++ src/main/java/ph/pcbuild/prototype/DATA.java
@@ -0,0 +1,13 @@
+package ph.pcbuild.prototype;
+
+/** Hard Coded Data
+ * Should be Moved to Test for user not to tinker on this, but will leave here because I don't want headache
+ */
+
+public class DATA {
+    //Computer Components
+    final static ComputerComponent c1 = new ComputerComponent(1, "Shopee", "RAM", "Kingston", "8GB DDR4 2400Mhz", 1870);
+    final static ComputerComponent c2 = new ComputerComponent(2, "Lazada", "RAM", "Kingston", "8GB DDR4 2400Mhz", 2000);
+    final static ComputerComponent c3 = new ComputerComponent(3, "Shopee", "CPU", "Intel", "i3 8100", 5650);
+
+}
