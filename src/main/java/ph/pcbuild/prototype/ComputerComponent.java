5588d9bffa5f19f2d329ca431bbbff54827d1b90
Eder Junior Chua <eder.chua@ciit.edu.ph>
2022-01-03 17:00:41 +0800

    added domain model for Computer Components

--- src/main/java/ph/pcbuild/prototype/ComputerComponent.java
+++ src/main/java/ph/pcbuild/prototype/ComputerComponent.java
@@ -1,4 +1,26 @@
 package ph.pcbuild.prototype;
 
+import static org.apache.commons.lang3.Validate.*;
+
 public class ComputerComponent {
+    private final int itemID;
+    private final String shop;
+    private final String component;
+    private final String brand;
+    private final String model;
+    private float price;
+
+
+    public ComputerComponent(int itemID, String shop, String component, String brand, String model, float price) {
+        notBlank(shop);
+        notBlank(component);
+        notBlank(brand);
+        notBlank(model);
+        this.itemID = itemID;
+        this.shop = shop;
+        this.component = component;
+        this.brand = brand;
+        this.model = model;
+        this.price = price;
+    }
 }
