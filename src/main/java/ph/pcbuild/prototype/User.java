5a1f3cea14ee5ed8dae6732b186fce149e010d2b
peejaypel <61832441+peejaypel@users.noreply.github.com>
2022-01-03 17:37:06 +0800

    added User class

---
+++ src/main/java/ph/pcbuild/prototype/User.java
@@ -0,0 +1,50 @@
+package ph.pcbuild.prototype;
+
+import java.util.List;
+import java.util.Objects;
+
+import static org.apache.commons.lang3.Validate.*;
+
+public class User {
+    private final int userId;
+    private final String firstName;
+    private final String lastName;
+    private List<ComputerComponent> Cart;
+
+    User(int userId, String firstName, String lastName){
+        isTrue(userId >= 0, "userId should be non-negative, was: " + userId);
+        notBlank(firstName);
+        notBlank(lastName);
+        this.userId = userId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    void addToCart(ComputerComponent component){
+        Cart.add(component);
+    }
+
+    void removeToCart(ComputerComponent component){
+        Cart.remove(component);
+    }
+
+    public int getUserId(){ return userId;}
+
+    @Override
+    public String toString() {
+        return "User #" + userId + " " + firstName + " " + lastName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return userId == user.userId;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(userId);
+    }
+}
