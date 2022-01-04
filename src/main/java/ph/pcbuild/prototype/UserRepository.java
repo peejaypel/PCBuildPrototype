aaa7504c8e53e8fe2d5e41da8270adbd35ca9a53
peejaypel <61832441+peejaypel@users.noreply.github.com>
2022-01-04 22:16:37 +0800

    added UserRepository class for finding user by userId

---
+++ src/main/java/ph/pcbuild/prototype/UserRepository.java
@@ -0,0 +1,13 @@
+package ph.pcbuild.prototype;
+
+import java.util.Collection;
+import static ph.pcbuild.prototype.DATA.*;
+import java.util.List;
+
+public class UserRepository {
+    public User findBy(int userId) {
+        return findAll().stream().filter(user -> user.getUserId() == userId).findFirst().orElse(JAMES);
+    }
+
+    public Collection<User> findAll() { return USERS;}
+}
