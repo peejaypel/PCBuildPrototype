e7dd80ea2e3ec40a353e14978b69775ffd53210f
Eder Junior Chua <eder.chua@ciit.edu.ph>
2022-01-05 00:01:10 +0800

    modify login for addToCart and created PCBuildPrototypeApplication class

---
+++ src/main/java/ph/pcbuild/prototype/PCBuildPrototypeApplication.java
@@ -0,0 +1,11 @@
+package ph.pcbuild.prototype;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PCBuildPrototypeApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PCBuildPrototypeApplication.class, args);
+    }
+}
