09d30a1ef771e1aa0419534dadca9e1a65295329
peejaypel <61832441+peejaypel@users.noreply.github.com>
2022-01-04 22:08:48 +0800

    added dummy data and HashSet for users

--- src/main/java/ph/pcbuild/prototype/DATA.java
+++ src/main/java/ph/pcbuild/prototype/DATA.java
@@ -4,14 +4,22 @@ import java.util.Collection;
 import java.util.HashSet;
 import java.util.List;
 
-/** Hard Coded Data
+/**
+ * Hard Coded Data
  * Should be Moved to Test for user not to tinker on this, but will leave here because I don't want headache
  */
 
 public class DATA {
     //Computer Components
     final static ComputerComponent C1 = new ComputerComponent(1, "Shopee", "RAM", "Kingston", "8GB DDR4 2400Mhz", 1870, 25);
-    final static ComputerComponent C2= new ComputerComponent(2, "Lazada", "RAM", "Kingston", "8GB DDR4 2400Mhz", 2000, 25);
+    final static ComputerComponent C2 = new ComputerComponent(2, "Lazada", "RAM", "Kingston", "8GB DDR4 2400Mhz", 2000, 25);
     final static ComputerComponent C3 = new ComputerComponent(3, "Shopee", "CPU", "Intel", "i3 8100", 5650, 25);
     final static Collection<ComputerComponent> COMPUTER_COMPONENTS = new HashSet<>(List.of(C1, C2, C3));
+
+    //Users
+    final static User JAMES = new User(1, "James", "Bond");
+    final static User BOB = new User(2, "Bob", "Sponge");
+    final static User WARD = new User(3, "Ward", "Squid");
+    final static Collection<User> USERS = new HashSet<>(List.of(JAMES, BOB, WARD));
+
 }
