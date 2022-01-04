f555f3530617b45c4f35152174f7ef91aeb29415
Eder Junior Chua <eder.chua@ciit.edu.ph>
2022-01-04 22:58:22 +0800

    draft for ComputerComponent Repository

---
+++ src/main/java/ph/pcbuild/prototype/ComputerComponentRepository.java
@@ -0,0 +1,13 @@
+package ph.pcbuild.prototype;
+
+import java.util.Collection;
+
+import static ph.pcbuild.prototype.DATA.*;
+
+public class ComputerComponentRepository {
+    public User findBy(int itemId) {
+        return findAll().stream().filter(component -> component.getItemId() == itemId).findFirst().orElse(C1);
+    }
+
+    public Collection<ComputerComponent> findAll() { return COMPUTER_COMPONENTS;}
+}
