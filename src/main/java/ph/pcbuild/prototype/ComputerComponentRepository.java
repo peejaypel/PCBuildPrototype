86da109f52c68adca3afd61291e98d51cb349b24
Eder Junior Chua <eder.chua@ciit.edu.ph>
2022-01-05 21:02:31 +0800

    remove default component since you don't want to sell default component if user can't find their components

--- src/main/java/ph/pcbuild/prototype/ComputerComponentRepository.java
+++ src/main/java/ph/pcbuild/prototype/ComputerComponentRepository.java
@@ -1,12 +1,13 @@
 package ph.pcbuild.prototype;
 
 import java.util.Collection;
+import java.util.Optional;
 
 import static ph.pcbuild.prototype.DATA.*;
 
 public class ComputerComponentRepository {
-    public ComputerComponent findBy(int itemId) {
-        return findAll().stream().filter(component -> component.getItemId() == itemId).findFirst().orElse(C1);
+    public Optional<ComputerComponent> findBy(int itemId) {
+        return findAll().stream().filter(component -> component.getItemId() == itemId).findFirst();
     }
 
     public Collection<ComputerComponent> findAll() { return COMPUTER_COMPONENTS;}
