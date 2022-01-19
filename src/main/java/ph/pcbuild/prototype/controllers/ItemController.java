9f70af56b7ad849973899fa716a7e245280b1690
peejaypel <61832441+peejaypel@users.noreply.github.com>
2022-01-20 01:01:21 +0800

    made item page template dynamic to each component

--- src/main/java/ph/pcbuild/prototype/controllers/ItemController.java
+++ src/main/java/ph/pcbuild/prototype/controllers/ItemController.java
@@ -4,26 +4,41 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-import ph.pcbuild.prototype.*;
+import ph.pcbuild.prototype.ComputerComponent;
+import ph.pcbuild.prototype.ComputerComponentRepository;
+import ph.pcbuild.prototype.User;
 
 @Controller
 @RequestMapping("item")
 @SessionAttributes("user")
 public class ItemController {
-    private UserRepository userRepo = new UserRepository();
+    private ComputerComponentRepository computerComponentRepo = new ComputerComponentRepository();
 
-    @ModelAttribute("user")
-    User user(@RequestParam Integer userId) {
-        if (userId == null) {
-            userId = 1; // default user
+    @ModelAttribute("component")
+    ComputerComponent component(@RequestParam Integer id) {
+        if (id == null) {
+            id = 1; // default user
         }
-        return userRepo.findBy(userId);
+        return computerComponentRepo.findBy(id);
     }
 
     @GetMapping
-    String showItem(Model model, Integer userId) {
+    String loadPage(Model model, Integer id) {
+        ComputerComponent component = id == null ? (ComputerComponent) model.getAttribute("component") : computerComponentRepo.findBy(id);
 
+        model.addAttribute("componentName", component.getComponent());
+        model.addAttribute("componentBrand", component.getBrand());
+        model.addAttribute("componentPrice", component.getPrice());
+        model.addAttribute("componentModel", component.getModel());
+        model.addAttribute("componentShop", component.getShop());
+        model.addAttribute("componentQuantity", component.getQuantity());
         return "item";
+    }
+
+    @PostMapping
+    String viewItem(@ModelAttribute User user, @RequestParam ComputerComponent component) {
+
+        return "redirect:item";
     }
 
 }
